57b318a6ebd84ac7516091e05dfabd6e9113401f
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-10-12 01:07:08 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Gitpub App!")
+} )
+
+
+app.listen(3000, ()=> {
+    console.log("we're listening")
+})
