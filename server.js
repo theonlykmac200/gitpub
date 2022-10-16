79cfdf63520b9d185e5afea0fa4a5129ecada72c
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-10-15 21:54:18 -0700

    added food menus

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const drinks = require("./models/drinks")
+const food = require("./models/food")
 
 const app = express()
 
@@ -19,6 +20,18 @@ app.get("/drinks/:id", (req, res) => {
         drinks: drinks[req.params.id]
     })
 })
+
+app.get("/food", (req, res) => {
+    res.render("food_index.ejs", {
+        allFood: food
+    })
+})
+app.get("/food/:id", (req, res) => {
+    res.render("food_show.ejs", {
+        food: food[req.params.id],
+    })
+})
+
 
 app.listen(3000, ()=> {
     console.log("we're listening")
