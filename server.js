a63d792b703ad1d5d9d8d1a4b759750080574215
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-10-15 20:54:45 -0700

    drinks index

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 } )
 
+app.get("/drinks", (req, res) => {
+    res.render("drinks_index.ejs", {
+        allDrinks: drinks
+    })
+})
+
 
 app.listen(3000, ()=> {
     console.log("we're listening")
